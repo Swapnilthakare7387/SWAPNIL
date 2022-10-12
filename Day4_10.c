7d3f7323dddb351c5914c0cc50a0c824c36beb30
Swapnilthakare7387 <114048367+Swapnilthakare7387@users.noreply.github.com>
2022-10-12 15:59:29 +0530

    Add files via upload

---
+++ Day4_10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main( )
+{
+    int num ; // declaration 
+    
+    // scanf( ); 
+
+    printf("Enter the number");
+    scanf("%d",&num);  
+    
+    // &address of operator 
+    printf("number  = %d",num); 
+    return 0; 
+}
