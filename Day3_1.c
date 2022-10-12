b9886bc88d77cf42dd629fce4972c23022cbb58c
Swapnilthakare7387 <114048367+Swapnilthakare7387@users.noreply.github.com>
2022-10-12 11:57:30 +0530

    Add files via upload

---
+++ Day3_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(   )
+{
+    printf("Hello world"); 
+    return 0; 
+}
+/*
+    1. Classwork 
+*/
+
+/*
+    warning ==> a.exe ( yes )
+
+    error  ==>  a.exe ( no )
+
+*/
